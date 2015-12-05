fe74307e9ef8b5b48df6fb70acc169097baafb18
Amit Gaur <agaur@amazon.com>
2015-12-04 16:44:43 -0800

    fix Item count helper

--- src/main/java/com/amitgaur/aws/ddb/tools/ItemCountTask.java
+++ src/main/java/com/amitgaur/aws/ddb/tools/ItemCountTask.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.google.common.util.concurrent.RateLimiter;
-
 import org.slf4j.Logger;
 
 import java.util.Collection;
@@ -13,6 +12,7 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -34,7 +34,6 @@ public class ItemCountTask implements Callable<Long> {
         this.mapper = new DynamoDBMapper(amazonDynamoDBClient);
         this.amazonDynamoDBClient = amazonDynamoDBClient;
         this.tableName = tableName;
-
         this.totalSegments = totalSegments;
         this.segmentNum = segmentNum;
         this.itemLimit = itemLimit;
@@ -50,39 +49,37 @@ public class ItemCountTask implements Callable<Long> {
         Map<String, AttributeValue> exclusiveStartKey1 = null;
         RateLimiter limiter = RateLimiter.create(itemLimit);
         ExecutorService parallelSegmentService = Executors.newFixedThreadPool(1);
+        CompareScanTask.ScanResultDecorator firstRequest = new CompareScanTask.ScanResultDecorator(this.amazonDynamoDBClient,this.tableName, null, this.isConsistentRead, this.itemLimit, this.totalSegments, this.segmentNum);
         try {
             while (true) {
                 limiter.acquire(itemLimit);
-
-                CompareScanTask.ScanResultDecorator firstRequest = new CompareScanTask.ScanResultDecorator(this.amazonDynamoDBClient,this.tableName, null, this.isConsistentRead, this.itemLimit, this.totalSegments, this.segmentNum);
-
                 ScanResult firstResult = parallelSegmentService.submit(firstRequest).get();
-
 
                 if (firstResult == null){
                     throw new Exception("Unable to get results from future");
                 }
-
                 Collection<Map<String, AttributeValue>> item1 = firstResult.getItems();
-
-
                 if (item1 != null) {
 
-                   numItems.getAndAdd(item1.size());
+                    numItems.getAndAdd(item1.size());
                 }
-
                 exclusiveStartKey1 = firstResult.getLastEvaluatedKey();
                 firstRequest.setExclusiveStartKey(exclusiveStartKey1);
 
                 if (exclusiveStartKey1 == null) {
-
-
                     break;
                 }
 
             }
         } catch (Exception e) {
             LOGGER.error("Error", e);
+        }
+        parallelSegmentService.shutdownNow();
+        try{
+            parallelSegmentService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        }
+        catch (InterruptedException e){
+
         }
         return numItems.longValue();
 
